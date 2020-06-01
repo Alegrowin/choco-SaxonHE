e051fcb469b213700f5424a48c7636cc06eeb52e
alegrowin <alexandre.proulx.3@gmail.com>
2020-06-01 10:24:38 -0400

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$basedir = "$env:ChocolateyInstall\bin\SaxonHE"
+$installArgs = '/sp- /verysilent /norestart /DIR="' + $basedir + '"'
+Install-ChocolateyPackage 'SaxonHE' 'exe' $installArgs 'https://versaweb.dl.sourceforge.net/project/saxon/Saxon-HE/9.9/SaxonHE9-9-1-7N-setup.exe'
+
+Install-ChocolateyPath "$basedir\bin"
